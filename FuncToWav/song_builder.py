ecc2371fe64d13fa061295bda05112f67a1f597b
Desktop <pyj4104@hotmail.com>
2021-04-08 04:04:01 -0400

    Fixed minor bugs

--- FuncToWav/song_builder.py
+++ FuncToWav/song_builder.py
@@ -1,6 +1,6 @@
 import numpy
 from extended_int import int_inf
-from functools import reduce 
+from functools import reduce
 from FuncToWav import constants as c
 from math import sin
 from FuncToWav.helper import heaviside, combineFuncs
@@ -46,17 +46,15 @@ class SongBuilder:
 
     def appendToSong(self, key: str, startTime: int, duration: int = None, endTime: int = int_inf):
         if (duration == None) and (endTime == int_inf):
-            endTime = int_inf
+            endTime = self.length
         elif (duration == None) and (endTime != int_inf):
             endTime = endTime
-            self.length = self.length if self.length >= startTime+duration else startTime+duration
         elif (duration != None) and (endTime == int_inf):
             endTime = startTime+duration
-            self.length = self.length if self.length >= startTime+duration else startTime+duration
         else:
-            if startTime+duration != endTime:
-                raise ValueError("the note duration + note start timing must match end timing")
+            raise ValueError("Either duration is specified or endTime is specified. Not Both.")
 
+        self.length = self.length if self.length >= endTime else endTime
         part = {
             c.FREQUENCY: self.numToFreq(self.keyToNum(key)),
             c.START_TIME: startTime,
@@ -72,4 +70,5 @@ class SongBuilder:
         return lambda x: sin(freq * x) * heaviside(x, sTime=sTime, eTime=eTime)
 
     def buildSong(self) -> Callable[[float], float]:
-        return reduce(combineFuncs, self.notes)
+        funcs = list(map(self.freqToFunc, self.notes))
+        return reduce(combineFuncs, funcs)
