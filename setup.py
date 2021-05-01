0c6251148d299fb535094708d75147f1910c8285
Desktop <pyj4104@hotmail.com>
2021-04-30 22:14:53 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="FuncToWav", # Replace with your own username
-    version="0.0.2",
+    version="1.0.0",
     author="YeounJun Park",
     author_email="pyj4104@hotmail.com",
     description="Given function, outputs wav file.",
